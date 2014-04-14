e10b66214fa982f0559316534b43ec98e0c5f240
Администратор <skiman@i.ua>
2014-04-14 18:00:10 +0300

    Two methods

--- MicHead/src/main/java/com/michead/michead/MainActivity.java
+++ MicHead/src/main/java/com/michead/michead/MainActivity.java
@@ -9,23 +9,32 @@ import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
     final String TAG = "myLogs";
+    boolean isRunning = false;
+    int audioformat = MediaRecorder.AudioEncoder.AMR_NB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        createAudioRecorder();
+    }
 
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        System.exit(0);
     }
 
     void createAudioRecorder() {
 
-        final int rateInHz = 24000;
+        new Thread(new Runnable() {
+            public void run() {
+        final int rateInHz = 8000;
         int minBufferSize = AudioRecord.getMinBufferSize(rateInHz,
                 AudioFormat.CHANNEL_CONFIGURATION_MONO,
                 AudioFormat.ENCODING_PCM_16BIT);
@@ -33,15 +42,10 @@ public class MainActivity extends Activity {
         AudioRecord audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC,
                 rateInHz,
                 AudioFormat.CHANNEL_CONFIGURATION_MONO,
-                AudioFormat.ENCODING_PCM_16BIT, minBufferSize);
+                audioformat, minBufferSize);
+        Log.d(TAG," audioRecord.getState()= "+audioRecord.getState());
         audioRecord.startRecording();
-        final byte audiobuffer[] = new byte[1200];
-
-        try {
-            Thread.sleep(150);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        final byte audiobuffer[] = new byte[minBufferSize];
 
         new Thread(new Runnable() {
             public void run() {
@@ -52,25 +56,91 @@ public class MainActivity extends Activity {
                 AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
                         rateInHz,
                         AudioFormat.CHANNEL_CONFIGURATION_MONO,
-                        AudioFormat.ENCODING_PCM_16BIT,
+                        audioformat,
                         bufferSize,
                         AudioTrack.MODE_STREAM
                 );
-                audioTrack.play();
-                while (true) {
-                    audioTrack.write(audiobuffer, 0, audiobuffer.length);
 
+                audioTrack.play();
+                while (isRunning) {
+                    audioTrack.write(audiobuffer, 0, audiobuffer.length);
                 }
+                audioTrack.release();
+                return;
             }
         }).start();
 
-
-        while (true) {
+        while (isRunning) {
             audioRecord.read(audiobuffer, 0, audiobuffer.length);
         }
-    }
+        audioRecord.release();
+        return;
+    }}).start();}
 
     public void sameStart(View view) {
+        Button btn = (Button)findViewById(R.id.startstop);
+        if (!isRunning){
+            createAudioRecorder();
+            isRunning = true;
+
+            btn.setText(getResources().getString(R.string.stop_text));
+            return;
+        }
+        isRunning = false;
+        btn.setText(getResources().getString(R.string.star_text));
+
+
+    }
+
+    private int freq =8000;
+    private AudioRecord audioRecord = null;
+    private AudioTrack audioTrack=null;
+    private Thread Rthread = null;
+
+    private AudioManager audioManager=null;
+    byte[] buffer = new byte[freq];
+
+    public void minDelay(View v){
+        android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_URGENT_AUDIO);
+        final int bufferSize = AudioRecord.getMinBufferSize(freq,
+                AudioFormat.CHANNEL_CONFIGURATION_MONO,
+                AudioFormat.ENCODING_PCM_16BIT);
+
+
+        audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC, freq,
+                AudioFormat.CHANNEL_CONFIGURATION_MONO,
+                MediaRecorder.AudioEncoder.AMR_NB, bufferSize);
+
+        audioTrack = new AudioTrack(AudioManager.ROUTE_HEADSET, freq,
+                AudioFormat.CHANNEL_CONFIGURATION_MONO,
+                MediaRecorder.AudioEncoder.AMR_NB, bufferSize,
+                AudioTrack.MODE_STREAM);
+
+
+
+        audioTrack.setPlaybackRate(freq);
+        final byte[] buffer = new byte[bufferSize];
+        audioRecord.startRecording();
+        Log.i("minDelay", "Audio Recording started");
+        audioTrack.play();
+        Log.i("minDelay", "Audio Playing started");
+        Rthread = new Thread(new Runnable() {
+            public void run() {
+                while (true) {
+                    try {
+                        audioRecord.read(buffer, 0, bufferSize);
+                        audioTrack.write(buffer, 0, buffer.length);
+
+                    } catch (Throwable t) {
+                        Log.e("Error", "Read write failed");
+                        t.printStackTrace();
+                    }
+                }
+            }
+        });
+        Rthread.start();
+
+
     }
 
 
